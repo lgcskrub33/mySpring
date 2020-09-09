1ef46dfceb28b3b753c47c4b364c80eee62a1979
cskrub33 <cskrub33@naver.com>
2020-09-09 22:33:59 +0900

    진격하라

---
+++ src/main/java/com/csksoft/springboot/web/IndexController.java
@@ -0,0 +1,47 @@
+package com.csksoft.springboot.web;
+
+import com.csksoft.springboot.config.auth.LoginUser;
+import com.csksoft.springboot.config.auth.dto.SessionUser;
+import com.csksoft.springboot.service.posts.PostsService;
+import com.csksoft.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user){
+        model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
+    }
+//    public String index(){
+//        return "index";
+//    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
+
+
+}
